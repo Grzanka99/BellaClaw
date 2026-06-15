041714fbd2dc0dcf31f2ad39992345d2ec2f5abc
Cezary Grzanka <cezarygrzanka@gmail.com>
2026-06-15 23:24:22 +0200

    fix: init messaging before signal setup

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import { SignalSingleton } from "./services/signal";
 async function init(): Promise<void> {
   console.time("init");
 
-  const migrationReady = MessagingAdapter.instance.migrateData();
-  const signalReady = SignalSingleton.instance.setup();
-
-  await Promise.all([migrationReady, signalReady]);
+  await MessagingAdapter.instance.migrateData();
+  await SignalSingleton.instance.setup();
   await MessagingAdapter.instance.setup();
 
   console.timeEnd("init");
